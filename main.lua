7e278cdca73742dfd0ca361237522cbafa58fea3
PeaPattern <72760536+PeaPattern@users.noreply.github.com>
2022-02-23 02:05:44 -0500

    Create main.lua
    full script

---
+++ main.lua
@@ -0,0 +1,202 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local deadretail = Instance.new("ScreenGui")
+local Background = Instance.new("ImageLabel")
+local titleBack = Instance.new("ImageLabel")
+local TextLabel = Instance.new("TextLabel")
+local Crash = Instance.new("TextButton")
+local Crash_Roundify_12px = Instance.new("ImageLabel")
+local Amount = Instance.new("TextBox")
+local Amount_Roundify_12px = Instance.new("ImageLabel")
+local Stop = Instance.new("TextButton")
+local Crash_Roundify_12px_2 = Instance.new("ImageLabel")
+local Credits = Instance.new("TextLabel")
+
+--Properties:
+
+deadretail.Name = "deadretail"
+deadretail.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+deadretail.ResetOnSpawn = false
+
+Background.Name = "Background"
+Background.Parent = deadretail
+Background.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Background.BackgroundTransparency = 1.000
+Background.Position = UDim2.new(0.859922171, 0, 0.566891253, 0)
+Background.Size = UDim2.new(0.0761673078, 0, 0.261693925, 0)
+Background.Image = "rbxassetid://3570695787"
+Background.ImageColor3 = Color3.fromRGB(20, 20, 20)
+Background.ScaleType = Enum.ScaleType.Slice
+Background.SliceCenter = Rect.new(100, 100, 100, 100)
+Background.SliceScale = 0.120
+
+titleBack.Name = "titleBack"
+titleBack.Parent = Background
+titleBack.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+titleBack.BackgroundTransparency = 1.000
+titleBack.Position = UDim2.new(0.0446998738, 0, 0.0225733556, 0)
+titleBack.Size = UDim2.new(0.916985929, 0, 0.107225426, 0)
+titleBack.Image = "rbxassetid://3570695787"
+titleBack.ImageColor3 = Color3.fromRGB(40, 40, 40)
+titleBack.ScaleType = Enum.ScaleType.Slice
+titleBack.SliceCenter = Rect.new(100, 100, 100, 100)
+titleBack.SliceScale = 0.120
+
+TextLabel.Parent = titleBack
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Size = UDim2.new(1, 0, 1.03551805, 0)
+TextLabel.Font = Enum.Font.SourceSansBold
+TextLabel.Text = "dead retail"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+
+Crash.Name = "Crash"
+Crash.Parent = Background
+Crash.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+Crash.BackgroundTransparency = 1.000
+Crash.BorderSizePixel = 0
+Crash.Position = UDim2.new(0.0383141786, 0, 0.166008443, 0)
+Crash.Size = UDim2.new(0.916983724, 0, 0.0888363346, 0)
+Crash.ZIndex = 2
+Crash.Font = Enum.Font.SourceSansBold
+Crash.Text = "crash"
+Crash.TextColor3 = Color3.fromRGB(255, 255, 255)
+Crash.TextScaled = true
+Crash.TextSize = 14.000
+Crash.TextWrapped = true
+
+Crash_Roundify_12px.Name = "Crash_Roundify_12px"
+Crash_Roundify_12px.Parent = Crash
+Crash_Roundify_12px.Active = true
+Crash_Roundify_12px.AnchorPoint = Vector2.new(0.5, 0.5)
+Crash_Roundify_12px.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Crash_Roundify_12px.BackgroundTransparency = 1.000
+Crash_Roundify_12px.Position = UDim2.new(0.5, 0, 0.5, 0)
+Crash_Roundify_12px.Selectable = true
+Crash_Roundify_12px.Size = UDim2.new(1, 0, 1, 0)
+Crash_Roundify_12px.Image = "rbxassetid://3570695787"
+Crash_Roundify_12px.ImageColor3 = Color3.fromRGB(30, 30, 30)
+Crash_Roundify_12px.ScaleType = Enum.ScaleType.Slice
+Crash_Roundify_12px.SliceCenter = Rect.new(100, 100, 100, 100)
+Crash_Roundify_12px.SliceScale = 0.120
+
+Amount.Name = "Amount"
+Amount.Parent = Background
+Amount.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+Amount.BackgroundTransparency = 1.000
+Amount.BorderSizePixel = 0
+Amount.Position = UDim2.new(0.0319284797, 0, 0.397204876, 0)
+Amount.Size = UDim2.new(0.916999996, 0, 0.0890000015, 0)
+Amount.ZIndex = 2
+Amount.Font = Enum.Font.SourceSansBold
+Amount.PlaceholderText = "crash amount here"
+Amount.Text = ""
+Amount.TextColor3 = Color3.fromRGB(255, 255, 255)
+Amount.TextScaled = true
+Amount.TextSize = 14.000
+Amount.TextWrapped = true
+
+Amount_Roundify_12px.Name = "Amount_Roundify_12px"
+Amount_Roundify_12px.Parent = Amount
+Amount_Roundify_12px.Active = true
+Amount_Roundify_12px.AnchorPoint = Vector2.new(0.5, 0.5)
+Amount_Roundify_12px.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Amount_Roundify_12px.BackgroundTransparency = 1.000
+Amount_Roundify_12px.Position = UDim2.new(0.5, 0, 0.5, 0)
+Amount_Roundify_12px.Selectable = true
+Amount_Roundify_12px.Size = UDim2.new(1, 0, 1, 0)
+Amount_Roundify_12px.Image = "rbxassetid://3570695787"
+Amount_Roundify_12px.ImageColor3 = Color3.fromRGB(25, 25, 25)
+Amount_Roundify_12px.ScaleType = Enum.ScaleType.Slice
+Amount_Roundify_12px.SliceCenter = Rect.new(100, 100, 100, 100)
+Amount_Roundify_12px.SliceScale = 0.120
+
+Stop.Name = "Stop"
+Stop.Parent = Background
+Stop.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+Stop.BackgroundTransparency = 1.000
+Stop.BorderSizePixel = 0
+Stop.Position = UDim2.new(0.0383141786, 0, 0.283698767, 0)
+Stop.Size = UDim2.new(0.916983724, 0, 0.0888363346, 0)
+Stop.ZIndex = 2
+Stop.Font = Enum.Font.SourceSansBold
+Stop.Text = "stop"
+Stop.TextColor3 = Color3.fromRGB(255, 255, 255)
+Stop.TextScaled = true
+Stop.TextSize = 14.000
+Stop.TextWrapped = true
+
+Crash_Roundify_12px_2.Name = "Crash_Roundify_12px"
+Crash_Roundify_12px_2.Parent = Stop
+Crash_Roundify_12px_2.Active = true
+Crash_Roundify_12px_2.AnchorPoint = Vector2.new(0.5, 0.5)
+Crash_Roundify_12px_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Crash_Roundify_12px_2.BackgroundTransparency = 1.000
+Crash_Roundify_12px_2.Position = UDim2.new(0.5, 0, 0.5, 0)
+Crash_Roundify_12px_2.Selectable = true
+Crash_Roundify_12px_2.Size = UDim2.new(1, 0, 1, 0)
+Crash_Roundify_12px_2.Image = "rbxassetid://3570695787"
+Crash_Roundify_12px_2.ImageColor3 = Color3.fromRGB(30, 30, 30)
+Crash_Roundify_12px_2.ScaleType = Enum.ScaleType.Slice
+Crash_Roundify_12px_2.SliceCenter = Rect.new(100, 100, 100, 100)
+Crash_Roundify_12px_2.SliceScale = 0.120
+
+Credits.Name = "Credits"
+Credits.Parent = Background
+Credits.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Credits.BackgroundTransparency = 1.000
+Credits.Position = UDim2.new(0.0446998775, 0, 0.875321865, 0)
+Credits.Size = UDim2.new(0.916985929, 0, 0.100000001, 0)
+Credits.Font = Enum.Font.SourceSansBold
+Credits.Text = "by peapattern xddd"
+Credits.TextColor3 = Color3.fromRGB(255, 255, 255)
+Credits.TextScaled = true
+Credits.TextSize = 14.000
+Credits.TextWrapped = true
+
+-- Scripts:
+
+local function ODUWL_fake_script() -- Background.Handler 
+	local script = Instance.new('LocalScript', Background)
+
+	local Player = game:GetService("Players").LocalPlayer
+	local Background = script.Parent
+	local Crash = Background:WaitForChild("Crash")
+	local Amount = Background:WaitForChild("Amount")
+	local Stop = Background:WaitForChild("Stop")
+	
+	local crashEnabled = false
+	
+	local function Crash(Amount)
+		for i=1,Amount do
+			game:GetService("ReplicatedStorage").Remotes.SwitchPlot:InvokeServer(Player)
+		end
+	end
+	
+	Crash.MouseButton1Down:Connect(function()
+		local txt = Amount.Text
+		if tonumber(txt) then
+			Crash(tonumber(txt))
+		end
+	end)
+	
+	Stop.MouseButton1Down:Connect(function()
+		crashEnabled = false
+	end)
+	
+end
+coroutine.wrap(ODUWL_fake_script)()
+local function WUQBT_fake_script() -- Background.GUIfix 
+	local script = Instance.new('LocalScript', Background)
+
+	script.Parent.Active = true
+	script.Parent.Draggable = true
+	
+end
+coroutine.wrap(WUQBT_fake_script)()
